aeefdebb706cabfaf6bbf5e4561db8835afae241
yesutkarsh <contactutkarshverma@gmail.com>
2024-12-14 19:28:50 +0530

    added lms

--- app/lms/components/UpcomingSession.jsx
+++ app/lms/components/UpcomingSession.jsx
@@ -4,12 +4,12 @@ export default function UpcomingSession() {
 
     let sessions = [
         {
-            title:"Node Js & Postman",
-            link:false,
+            title:"DOM & Firebase",
+            link:"https://meet.google.com/znf-ecop-ouk",
             instructor:"Utkarsh Verma",
-            date:"December 15",
-            timing:"02:00",
-            day:"Sunday"
+            date:"December 14",
+            timing:"07:38",
+            day:"Saturday"
         },
         {
             title:"Stack DSA",
@@ -22,14 +22,14 @@ export default function UpcomingSession() {
     ]
   return (
     <>
-        <div className={style.heading}>Upcoming Sessions </div>
+        <div className={style.heading}>Upcoming Sessionss </div>
     {
         sessions.map((session,index)=>{
             return(
                 <div key={index} className={style.sessionCard}>
                     <span>{session.title}</span>
                     <span>{"Instructor: "+session.instructor}</span>
-                    <span>{session.link == false?`Link Will Be Available at ${session.timing} on ${session.day + " "+session.date}`:"Join Now"}</span>
+                    <span>{session.link == false?`Link Will Be Available at ${session.timing} on ${session.day + " "+session.date}`:<><a style={{color:"#469732"}} href={session.link}>Join Now</a></>}</span>
                 </div>
             )
         })
